f4b8a30545a1ec5b2b876a5b0e42bbad1c6d4331
George Lansdown <george@lansdown.me>
2018-12-17 18:58:17 +0000

    Add files via upload

--- RADNAMES.py
+++ RADNAMES.py
@@ -2,12 +2,22 @@ def generate(name):
 	name = name.split(" ")
 	newName = ""
 	for n in name:
-		if n[0].lower() not in ['a','e','i','o','u']:
-			newName += n[0]
-		newName += "onk "
+		if n.split("-") != n:
+			for f in n.split("-"):
+				if f[0].lower() not in ['a','e','i','o','u']:
+					newName += f[0]
+				newName += "onk "
+		else:
+			if n[0].lower() not in ['a','e','i','o','u']:
+				newName += n[0]
+				newName += "onk "
 	return newName
 
-names = ["Michael Aylesbury", "George Lansdown", "Jen Logan", "Liana Ahmed", "Jacob Rees Mog", "Orla Innes"]
-for f in names:
-	print f, generate(f)
-		
+names = []
+for line in open('names', 'r').readlines():
+	if line.strip() == '':
+		continue
+	names.append(line)
+
+for n in names:
+	print n, generate(n)
